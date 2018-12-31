08ab074df451ba18e677b8f78980ac0485153eb6
Lucas Huber <lucas.huber@org-it.ch>
2018-12-31 17:37:35 +0100

    [FIX] SaaS portal small fix

--- saas_portal/models/__init__.py
+++ saas_portal/models/__init__.py
@@ -2,6 +2,5 @@ from . import res_config
 from . import saas_portal_server
 from . import saas_portal_plan
 from . import saas_portal_client
-from . import saas_portal_plan_topup
 from . import ir_config_parameter
 from . import res_users
--- saas_portal/models/saas_portal_plan_topup.py
+++
@@ -1,7 +0,0 @@
-from odoo import api, exceptions, fields, models
-
-
-class SaasPortalPlanTopup (models.Model):
-    _name = 'saas_portal.plan_topup'
-
-    name = fields.Char('Topup name')
