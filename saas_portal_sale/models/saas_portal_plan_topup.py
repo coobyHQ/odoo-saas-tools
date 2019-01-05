bbe3c37cb5b7e4e8034c6c31f2d0646b62f5c785
Antonio Buric <antonio.burich@gmail.com>
2019-01-05 09:26:54 +0100

    [IMP] Shop workflow logic improvements and contract integration.

--- saas_portal_sale/models/saas_portal_plan_topup.py
+++ saas_portal_sale/models/saas_portal_plan_topup.py
@@ -8,5 +8,6 @@ class SaasPortalPlanTopup (models.Model):
     name = fields.Char('Name', required=True)
     product_tmpl_id = fields.Many2one('product.template', 'Product Template', domain=[('saas_product_type', '=', 'addon')], required=True)
     plan_id = fields.Many2one('saas_portal.plan', string='Plan', required=True)
+    contract_template_id = fields.Many2one('account.analytic.contract', string='Contract Template')
     topup_users = fields.Integer('# of Extra Users', default=1)
     topup_storage = fields.Integer('# of Extra Storage (MB)', default=0)
