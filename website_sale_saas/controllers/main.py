56d3ab21462e77c736b32759d303f1c223acdc3e
Antonio Buric <antonio.burich@gmail.com>
2019-01-05 18:45:12 +0100

    [IMP] Show warning when ordering more then one base SaaS product.

--- website_sale_saas/controllers/main.py
+++ website_sale_saas/controllers/main.py
@@ -158,6 +158,97 @@ class SaasCreateInstanceAfterValidating(WebsiteSale):
 
         return request.render("website_sale.extra_info", values)
 
+    @http.route(['/shop/checkout'], type='http', auth="public", website=True)
+    def checkout(self, **post):
+        order = request.website.sale_get_order()
+
+        if order and order.order_line.mapped('product_id').filtered(lambda product: product.saas_plan_id != False and product.saas_product_type == 'base'):
+            lines = order.order_line.filtered(lambda line: line.product_id.saas_plan_id != False and line.product_id.saas_product_type == 'base')
+            for line in lines:
+                if line.product_uom_qty > 1:
+                    return request.redirect('/shop/cart/')
+
+        redirection = self.checkout_redirection(order)
+        if redirection:
+            return redirection
+
+        if order.partner_id.id == request.website.user_id.sudo().partner_id.id:
+            return request.redirect('/shop/address')
+
+        for f in self._get_mandatory_billing_fields():
+            if not order.partner_id[f]:
+                return request.redirect('/shop/address?partner_id=%d' % order.partner_id.id)
+
+        values = self.checkout_values(**post)
+
+        values.update({'website_sale_order': order})
+
+        # Avoid useless rendering if called in ajax
+        if post.get('xhr'):
+            return 'ok'
+        return request.render("website_sale.checkout", values)
+
+    @http.route(['/shop/cart'], type='http', auth="public", website=True)
+    def cart(self, access_token=None, revive='', **post):
+        """
+        Main cart management + abandoned cart revival
+        access_token: Abandoned cart SO access token
+        revive: Revival method when abandoned cart. Can be 'merge' or 'squash'
+        """
+        order = request.website.sale_get_order()
+        if order and order.state != 'draft':
+            request.session['sale_order_id'] = None
+            order = request.website.sale_get_order()
+        values = {}
+        if access_token:
+            abandoned_order = request.env['sale.order'].sudo().search([('access_token', '=', access_token)], limit=1)
+            if not abandoned_order:  # wrong token (or SO has been deleted)
+                return request.render('website.404')
+            if abandoned_order.state != 'draft':  # abandoned cart already finished
+                values.update({'abandoned_proceed': True})
+            elif revive == 'squash' or (revive == 'merge' and not request.session[
+                'sale_order_id']):  # restore old cart or merge with unexistant
+                request.session['sale_order_id'] = abandoned_order.id
+                return request.redirect('/shop/cart')
+            elif revive == 'merge':
+                abandoned_order.order_line.write({'order_id': request.session['sale_order_id']})
+                abandoned_order.action_cancel()
+            elif abandoned_order.id != request.session[
+                'sale_order_id']:  # abandoned cart found, user have to choose what to do
+                values.update({'access_token': abandoned_order.access_token})
+
+        if order:
+            from_currency = order.company_id.currency_id
+            to_currency = order.pricelist_id.currency_id
+            compute_currency = lambda price: from_currency.compute(price, to_currency)
+        else:
+            compute_currency = lambda price: price
+
+        valid = True
+        if order and order.order_line.mapped('product_id').filtered(lambda product: product.saas_plan_id != False and product.saas_product_type == 'base'):
+            lines = order.order_line.filtered(lambda line: line.product_id.saas_plan_id != False and line.product_id.saas_product_type == 'base')
+            for line in lines:
+                if line.product_uom_qty > 1:
+                    valid = False
+
+        values.update({
+            'website_sale_order': order,
+            'compute_currency': compute_currency,
+            'suggested_products': [],
+            'valid': valid,
+        })
+        if order:
+            _order = order
+            if not request.env.context.get('pricelist'):
+                _order = order.with_context(pricelist=order.pricelist_id.id)
+            values['suggested_products'] = _order._cart_accessories()
+
+        if post.get('type') == 'popover':
+            # force no-cache so IE11 doesn't cache this XHR
+            return request.render("website_sale.cart_popover", values, headers={'Cache-Control': 'no-cache'})
+
+        return request.render("website_sale.cart", values)
+
     @http.route('/shop/payment/validate', type='http', auth="public", website=True)
     def payment_validate(self, transaction_id=None, sale_order_id=None, **post):
         """ Method that should be called by the server when receiving an update
