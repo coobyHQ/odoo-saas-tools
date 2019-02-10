968cb6fe3ba293f4397c4d3ee8d401b46882388d
Antonio <antonio.burich@gmail.com>
2019-02-10 23:50:47 +0100

    [IMP] Helpdesk integration details.

--- saas_portal_helpdesk/models/saas_portal_client.py
+++ saas_portal_helpdesk/models/saas_portal_client.py
@@ -8,28 +8,51 @@ class SaasPortalClient(models.Model):
     _inherit = 'saas_portal.client'
 
     @api.multi
-    @api.depends('ticket_ids')
-    def _get_number_of_open_tickets(self):
+    @api.depends('ticket_ids', 'ticket_ids.state')
+    def _get_number_of_tickets(self):
+        closed_states = [
+            self.env.ref('website_support.website_ticket_state_customer_closed', raise_if_not_found=False),
+            self.env.ref('website_support.website_ticket_state_staff_closed', raise_if_not_found=False)
+        ]
         for ticket in self:
-            ticket.ticket_nr_open = len(ticket.ticket_ids)
-            # Todo   ticket.ticket_nr_open = len(ticket.ticket_ids.filtered(lambda c: c.states.id <= 6))
-
-    @api.multi
-    @api.depends('ticket_ids')
-    def _get_number_of_closed_tickets(self):
-        for ticket in self:
-            ticket.ticket_nr_open = len(ticket.ticket_ids)
-            # ticket.ticket_nr_open = len(ticket.ticket_ids.filtered(lambda c: c.states.id >= 7))
-
-    @api.multi
-    def _get_number_of_tickets_text(self):
-        for ticket in self:
-            open_ticket = ticket.ticket_nr_open
-            closed = ticket.ticket_nr_closed
-            ticket.ticket_nr_text = "%s / %s" % (open_ticket, closed)
+            ticket.ticket_nr_open = len(ticket.ticket_ids.filtered(lambda c: c.state not in closed_states))
+            ticket.ticket_nr_closed = len(ticket.ticket_ids.filtered(lambda c: c.state in closed_states))
+            ticket.ticket_nr_text = "%s / %s" % (ticket.ticket_nr_open, ticket.ticket_nr_closed)
 
     ticket_ids = fields.One2many('website.support.ticket', 'saas_client_id', string='Support Tickets', readonly=False)
-    ticket_nr_open = fields.Integer(string='# of open Tickets', compute=_get_number_of_open_tickets)
-    ticket_nr_closed = fields.Integer(string='# of closed Tickets', compute=_get_number_of_closed_tickets)
-    ticket_nr_text = fields.Char('# of Tickets', compute=_get_number_of_tickets_text)
+    ticket_nr_open = fields.Integer(string='# of open Tickets', compute=_get_number_of_tickets)
+    ticket_nr_closed = fields.Integer(string='# of closed Tickets', compute=_get_number_of_tickets)
+    ticket_nr_text = fields.Char('# of Tickets', compute=_get_number_of_tickets)
     ticket_latest_id = fields.Many2one('website.support.ticket', string='Latest Ticket', readonly=True)
+
+    @api.multi
+    def action_show_open_tickets(self):
+        closed_states = [
+            self.env.ref('website_support.website_ticket_state_customer_closed', raise_if_not_found=False),
+            self.env.ref('website_support.website_ticket_state_staff_closed', raise_if_not_found=False)
+        ]
+        tickets = self.ticket_ids.filtered(lambda c: c.state not in closed_states)
+        action = self.env.ref('website_support.website_support_ticket_action_partner').read()[0]
+        if tickets and action:
+            action['views'] = [(self.env.ref('website_support.website_support_ticket_view_tree').id, 'tree'),
+                               (self.env.ref('website_support.website_support_ticket_view_form').id, 'form')]
+            action['domain'] = [('id', 'in', tickets.ids)]
+        else:
+            action = {'type': 'ir.actions.act_window_close'}
+        return action
+
+    @api.multi
+    def action_show_closed_tickets(self):
+        closed_states = [
+            self.env.ref('website_support.website_ticket_state_customer_closed', raise_if_not_found=False),
+            self.env.ref('website_support.website_ticket_state_staff_closed', raise_if_not_found=False)
+        ]
+        tickets = self.ticket_ids.filtered(lambda c: c.state in closed_states)
+        action = self.env.ref('website_support.website_support_ticket_action_partner').read()[0]
+        if tickets and action:
+            action['views'] = [(self.env.ref('website_support.website_support_ticket_view_tree').id, 'tree'),
+                               (self.env.ref('website_support.website_support_ticket_view_form').id, 'form')]
+            action['domain'] = [('id', 'in', tickets.ids)]
+        else:
+            action = {'type': 'ir.actions.act_window_close'}
+        return action
--- saas_portal_helpdesk/models/website_support_ticket.py
+++ saas_portal_helpdesk/models/website_support_ticket.py
@@ -10,4 +10,15 @@ class WebsiteSupportTicket(models.Model):
     saas_client_id = fields.Many2one('saas_portal.client', string='SaaS Client Instance')
     saas_client_ident = fields.Char(related='saas_client_id.identifier', string='Identifier')
 
+    @api.multi
+    def action_open_saas_client(self):
+        saas_client = self.saas_client_id
+        action = self.env.ref('saas_portal.action_clients').read()[0]
+        if saas_client and action:
+            action['views'] = [(self.env.ref('saas_portal.view_clients_form').id, 'form')]
+            action['res_id'] = saas_client.id
+        else:
+            action = {'type': 'ir.actions.act_window_close'}
+        return action
+
 
