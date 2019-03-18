3c6c68f5242fb40a04add193d237d45fb28c887f
Lucas Huber <lucas.huber@org-it.ch>
2019-03-18 09:25:56 +0100

    [IMP] SaaS portal, Wizard adding Template DB adding more fields.

--- saas_portal/wizard/add_existing_template.py
+++ saas_portal/wizard/add_existing_template.py
@@ -3,6 +3,7 @@ import random
 import string
 from odoo import api, fields, models
 from odoo.tools.translate import _
+from odoo.tools import scan_languages
 from odoo.exceptions import ValidationError
 import logging
 _logger = logging.getLogger(__name__)
@@ -14,15 +15,21 @@ class SaasPortalAddExistingDatabase(models.TransientModel):
     """
     _name = 'saas_portal.add_existing_template'
 
-    name = fields.Char(string='Subdomain', required=True)
+    @api.model
+    def _get_default_name_txt(self):
+        return "%s %s" % (self.subdomain or '', self.db_primary_lang or '')
+
+    subdomain = fields.Char(string='Subdomain', required=False)
+    name_txt = fields.Char('Name', required=True, default=_get_default_name_txt)
     server_id = fields.Many2one('saas_portal.server', string='Server', required=True)
     domain = fields.Char(related='server_id.name', string='Server Domain', readonly=True)
+    db_primary_lang = fields.Selection(scan_languages(), 'Database primary language')
     plan_id = fields.Many2one('saas_portal.plan', string='Add To Plan')
     password = fields.Char(required=True)
 
     @api.multi
     def add(self):
-        new_db = self.name + '.' + self.server_id.domain
+        new_db = self.subdomain + '.' + self.server_id.domain
         saas_portal_database = self.env['saas_portal.database']
         if saas_portal_database.search([('name', '=', new_db)]):
             raise ValidationError(
@@ -30,14 +37,16 @@ class SaasPortalAddExistingDatabase(models.TransientModel):
                   "'%s'") % new_db
             )
         vals = {
-            'subdomain': self.name,
+            'subdomain': self.subdomain,
+            'name_txt': self.name_txt,
             'server_id': self.server_id.id,
             'state': 'template',
             'db_type': 'template',
+            'db_primary_lang': self.db_primary_lang,
             'password': self.password,
         }
         if self.plan_id:
-            vals.update(plan_ids = [(4, self.plan_id.id)])
+            vals.update(plan_ids=[(4, self.plan_id.id)])
 
         new_template = saas_portal_database.create(vals)
         if self.plan_id and not self.plan_id.template_id:
